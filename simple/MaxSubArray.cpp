7bdfe3d33c9424313c1570899b1a926d179ee2d7
wd <ddwwzzyy@gmail.com>
2018-10-22 10:52:20 -0400

    maxsubarray

---
+++ simple/MaxSubArray.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums) {
+        std::vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 0; i < nums.size(); ++i) {
+            dp[i] = nums[i] > nums[i] + dp[i-1] ? nums[i] : nums[i] + dp[i-1];
+            max = max > dp[i] ? max : dp[i];
+        }
+        return max;
+    }
+};
