57d92b837affb9c258caed99edc72741ac1851c4
wd <ddwwzzyy@gmail.com>
2019-01-31 19:03:18 -0500

    asldf

---
+++ medium/zigzagconversion.cpp
@@ -0,0 +1,15 @@
+/*
+ * ZigZag Conversion
+ *
+ * Date: 01.31.2019
+ */
+
+class Solution {
+public:
+    string convert(string s, int numRows)
+    {
+        vector<string> s.reserve(numRows);
+        for (int i = 0; i < )
+
+    }
+};
