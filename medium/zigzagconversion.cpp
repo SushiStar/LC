398860d0c9ee58b901eef66faf438d891dbf2581
Wayne Du <ddwwzzyy@gamil.com>
2019-03-02 00:51:02 -0500

    ZigZag solved, easier than I thought

--- medium/zigzagconversion.cpp
+++ medium/zigzagconversion.cpp
@@ -6,10 +6,19 @@
 
 class Solution {
 public:
-    string convert(string s, int numRows)
-    {
-        vector<string> s.reserve(numRows);
-        for (int i = 0; i < )
+    string convert(string s, int numRows) {
+        std::vector<string> contain;
+        for (auto i = 0; i < numRows; ++i) contain.push_back("");
 
+        if (numRows == 1) return s;
+        int bound = numRows * 2 - 2;
+        for (int i = 0; i < s.size(); ++i) {
+            int k = i % bound;
+            k = std::min(k, bound - k);
+            contain.at(k) += s.at(i);
+        }
+        std::string res{""};
+        for (auto &m : contain) res += m;
+        return res;
     }
 };
