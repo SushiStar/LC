54a2ef402ccc44a07b8104313a948106fc6df0a5
Wayne Du <ddwwzzyy@gamil.com>
2019-03-04 00:48:38 -0500

    tough one. Need to take care of the duplicates.

---
+++ medium/3Sum.cpp
@@ -0,0 +1,44 @@
+/*
+ * Given an array nums of n integers, are there a, b, c in nums
+ * Such that a+b+c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ *
+ * Similar to 2sum method, fix one and find the rest two(sort and find sum of 2
+ * ends). No need to look back because that situation is already consisdered.
+ *
+ * Date: March/03/2019
+ *
+ * Autor: Wei Du
+ *
+ */
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res{};
+
+        if (nums.size() < 3) return res;
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int goal = -nums.at(i);
+            int l(i + 1);
+            int r(nums.size() - 1);
+            while (l < r) {
+                if (nums.at(l) + nums.at(r) == goal) {
+                    vector<int> temp{nums.at(i), nums.at(l), nums.at(r)};
+                    res.push_back(temp);
+                    //--r; ++l;
+                    while (l < r && temp.at(2) == nums.at(r)) --r;
+                    while (l < r && temp.at(1) == nums.at(l)) ++l;
+                } else if (nums.at(l) + nums.at(r) > goal) {
+                    --r;
+                } else {  // nums.at(l) + nums.at(r) < goal
+                    ++l;
+                }
+            }
+
+            while (i < nums.size() - 2 && nums.at(i) == nums.at(i + 1)) ++i;
+        }
+        return res;
+    }
+};
