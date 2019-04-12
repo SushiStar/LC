330c2ada1bf4d6fa1611c12ebd8eb5d62de90c23
Wayne Du <ddwwzzyy@gamil.com>
2019-04-12 11:17:29 -0400

    DP get

--- medium/PerfectSquare.cpp
+++ medium/PerfectSquare.cpp
@@ -5,11 +5,51 @@
  * Date: Apr/12/2019
  * Author: Wei Du
  */
-#include<cmath>
+#include <cmath>
+#include <unordered_map>
 
 class Solution {
 public:
     int numSquares(int n) {
+        std::unordered_map<int, int> mp;
+        return numSquares(n, mp);
+    }
 
+private:
+    int numSquares(int n, std::unordered_map<int, int>& mp) {
+        if (!n) return 0;
+        int ceil = (int)floor(sqrt(n));
+        int minn(INT_MAX);
+        int lower;
+        for (int i = 1; i <= ceil; ++i) {
+            if (mp[n - i * i] != 0) {
+                lower = mp[n - i * i];
+            } else {
+                lower = numSquares(n - i * i, mp);
+                mp[n - i * i] = lower;
+            }
+            minn = minn < lower ? minn : lower;
+        }
+        return minn + 1;
+    }
+};
+
+class Solution {
+public:
+    int numSquares(int n) {
+        if (!n) return 0;
+        std::vector<int> mp(n+1);
+        mp[0] = 0;
+        mp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            mp[i] = INT_MAX;
+            int j = 1;
+            while (j*j < i) {
+                mp[i] = min(mp[i], mp[i-j*j]);
+                ++j;
+            }
+            ++mp[i];
+        }
+        return mp[n];
     }
 };
