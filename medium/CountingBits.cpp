f1e6c14c7021483656f6fb9a61b918a36c77ea91
Wayne Du <ddwwzzyy@gamil.com>
2019-04-18 22:09:24 -0400

    learned a good lesson from priority_queue and lambda comparator

---
+++ medium/CountingBits.cpp
@@ -0,0 +1,42 @@
+/*
+ * Given a non-negative integer number num. For every numbers i
+ * in the range 0<=i<=num, calculate the nubmer of 1's in their
+ * binary representation and return them as an array.
+ *
+ * Date: Apr/18/2019
+ * Author: Wei Du
+ */
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        std::vector<int> res{0};
+        if (!num) return res;
+        int k = (int)(std::log2(num) + 0.05);
+        for (int i = 0; i < k; ++i) {
+            std::vector<int> newres(res);
+            for (int j = 0; j < newres.size(); ++j) ++newres[j];
+            res.insert(res.end(), newres.begin(), newres.end());
+        }
+        // pow(2,k) is not included in the vector;
+        int rst = num - std::pow(2, k) + 1;
+        if (rst) {
+            std::vector<int> rest;
+            for (int i = 0; i < rst; ++i) {
+                rest.push_back(res[i] + 1);
+            }
+            res.insert(res.end(), rest.begin(), rest.end());
+        }
+        return res;
+    }
+
+    vector<int> countBits(int num) {
+        std::vector<int> res(num+1,0);
+        for (int i = 1; i < res.size(); ++i) {
+            res[i] = res[i&(i-1)]+1;
+        }
+        return res;
+    }
+};
--- medium/HouseRobberIII.cpp
+++ medium/HouseRobberIII.cpp
@@ -1,8 +1,8 @@
 /*
- * The thief has found himself a new place for his thievery again. There 
+ * The thief has found himself a new place for his thievery again. There
  * is only one entrance to this area, called the "root." Besides the root,
- * each house has one and only one parent house. After a tour, the smart 
- * thief realized that "all houses in this place forms a binary tree". It 
+ * each house has one and only one parent house. After a tour, the smart
+ * thief realized that "all houses in this place forms a binary tree". It
  * will automatically contact the police if two directly-linked houses were
  * broken into on the same night.  Determine the maximum amount of money the
  * thief can rob tonight without alerting the police.
@@ -18,11 +18,24 @@
  * Date: Apr/18/2019
  * Author: Wei Du
  */
-
+#include<array>
 class Solution {
 public:
     int rob(TreeNode* root) {
         if (!root) return 0;
+        auto res = robN(root);
+        return std::max(res[0], res[1]);
+    }
 
+private:
+    std::array<int,2> robN(TreeNode* root) {
+        // Considering children and grand children
+        std::array<int,2> res = {0,0};
+        if (!root) return res;
+        auto left = robN(root->left);
+        auto right = robN(root->right);
+        res[0] = std::max(left[0], left[1])+ std::max(right[0], right[1]);
+        res[1] = root->val + left[0] + right[0];
+        return res;
     }
 };
