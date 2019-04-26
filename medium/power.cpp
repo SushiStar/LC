95b769e9dcd33e6cb1c6b8ffc1d30342cc594cf3
Wayne Du <ddwwzzyy@gamil.com>
2019-04-25 23:30:18 -0400

    ridiculous

---
+++ medium/power.cpp
@@ -0,0 +1,14 @@
+/*
+ * Implement pow(x,y), which calculates x raised to the power n. 
+ *
+ * Date: Apr/25/2019
+ * Author: Wei Du
+ */
+#include <cmath>
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
