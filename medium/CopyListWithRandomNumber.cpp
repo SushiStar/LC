b88472c356f1f8032b3794ad45aae5550a812fbf
Wayne Du <ddwwzzyy@gamil.com>
2019-05-12 22:45:16 -0400

    all

---
+++ medium/CopyListWithRandomNumber.cpp
@@ -0,0 +1,55 @@
+/*
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ *
+ * Return a deep copy of the list.
+ *
+ * class Node {
+ * public:
+ *      int val;
+ *      Node* next;
+ *      Node* random;
+ *
+ *      Node() {}
+ *
+ *      Node(int _val, Node* _next, Node* _random) {
+ *          val = _val;
+ *          next = _next;
+ *          random = _random;
+ *      }
+ * };
+ *
+ * Date: May/12/2019
+ * Author: Wei Du
+ *
+ */
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+        std::vector<Node*> vct;
+        std::unordered_map<Node*,int> mp;
+        int counter(0);
+        while (head) {
+            vct.push_back(new Node(head->val, nullptr, head->random));
+            mp[head] = counter;
+            head = head->next;
+            ++counter;
+        }
+
+        int end(vct.size()-1);
+        for (int i = 0; i < end; ++i) {
+            vct[i]->next = vct[i+1];
+            if (vct[i]->random) {
+                vct[i]->random = vct[mp[vct[i]->random]];
+            }
+        }
+        if (vct[end]->random) {
+            vct[end]->random = vct[mp[vct[end]->random]];
+        }
+        return vct[0];
+    }
+};
