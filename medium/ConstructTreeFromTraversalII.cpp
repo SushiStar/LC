4c7a6d1977a827604f319eda4058efeda621e9f4
Wayne Du <ddwwzzyy@gamil.com>
2020-02-01 20:40:58 -0500

    ..

---
+++ medium/ConstructTreeFromTraversalII.cpp
@@ -0,0 +1,23 @@
+/*
+ * Given inorder and postorder traversal of a tree, construct the binary tree
+ * You may assume duplicates may not assume in the tree.
+ *
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ *
+ * Date: Jun/19/2019
+ * Author: Wei Du
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.empty()) return nullptr;
+
+    }
+};
