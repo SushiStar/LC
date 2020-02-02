be9278a0b214de4302caca0e150b63e87c37bc4c
Wayne Du <ddwwzzyy@gamil.com>
2020-02-01 22:40:02 -0500

    1, 2

---
+++ easy/TwoSum.cpp
@@ -0,0 +1,23 @@
+/*
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target. You may assume that each input would have
+ * exactly one solution, and you may not use the the same element twice.
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        std::unordered_map<int, int> num_idx;
+        std::vector<int> rlt;
+        for (int i = 0; i < nums.size(); ++i) {
+            int comp = target - nums[i];
+            if (num_idx.find(comp) == num_idx.end())
+                num_idx[nums[i]] = i;
+            else {
+                rlt.push_back(i);
+                rlt.push_back(num_idx[comp]);
+                return rlt;
+            }
+        }
+    }
+};
--- medium/AddTwoNumbers.cpp
+++ medium/AddTwoNumbers.cpp
@@ -1,25 +1,37 @@
 /**
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
  * Definition for singly linked list.
  * struct ListNode {
  *      int val;
  *      ListNode *next;
- *      ListNOde(int x) : val(x), next(NULL) {}
+ *      ListNode(int x) : val(x), next(NULL) {}
  * };
  */
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode preHead(0), *p = &preHead;
-        int extra = 0;
-        while (l1 || l2 || extra) {
-            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *rlt = l1, prel1(nullptr);
+        int extra(0);
+        while (extra || l2) {
+            if (!l1) {
+                l1 = new ListNode(0);
+                prel1->next =  l1;
+            }
+            int sum = extra + l1->val + (l2 ? l2->val : 0);
             extra = sum / 10;
-            p->next = new ListNode(sum % 10);
-            p = p->next;
-            l1 = l1 ? l1->next : l1;
+            l1->val = sum % 10;
+
+            prel1 = l1;
+            l1 = l1->next;
             l2 = l2 ? l2->next : l2;
         }
-        return preHead.next;
+        return rlt;
     }
 };
