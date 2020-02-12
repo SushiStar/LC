37bac63411d7ca6812ffee4ec2f7b3cca15889e5
Wayne Du <ddwwzzyy@gamil.com>
2020-02-11 23:28:34 -0500

    start leetcode, LongestSubstring improved

--- medium/LongestSubstring.cpp
+++ medium/LongestSubstring.cpp
@@ -1,13 +1,31 @@
+/*
+ * Given a  string, find the length of the longest substring without
+ * repeating characters.
+ *
+ * Hint: sliding window, and be careful how much the left pointer
+ * could be updated.
+ *
+ * Author: Wei Du
+ * Date: 02/11/2020
+ *
+ */
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); ++i) {
-            if (dict[s[i]] > start) start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+        if (s.empty()) return 0;
+        int il(0), ir(1), maxLength(1);
+        std::array<int, 128> tb;
+        tb.fill(-1);
+        tb[s[0]] = 0;
+        while (ir < s.szie()) {
+            if (tb[s[ir]] != -1) {
+                il = std::max(il, tb[s[ir]] + 1);
+            }
+            tb[s[ir]] = ir;
+            maxLength = std::max(maxLength, ir - il + 1);
+            ++ir;
         }
-        return maxLen;
+        return maxLength;
     }
 };
