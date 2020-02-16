e3d884e966870aec104aad1815131978c891518f
Wayne Du <ddwwzzyy@gamil.com>
2020-02-15 23:30:07 -0500

    relearned regular expression

--- hard/RegularExpressionMatching.cpp
+++ hard/RegularExpressionMatching.cpp
@@ -9,13 +9,13 @@
  *
  * 1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
  * 2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
- * 3, If p.charAt(j) == '*':
- * here are two sub conditions:
- *      1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this
- * case, a* only counts as empty 2   if p.charAt(j-1) == s.charAt(i) or
- * p.charAt(j-1) == '.': dp[i][j] = dp[i-1][j]    //in this case, a* counts as
- * multiple a or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
- *          or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+ * 3, If p.charAt(j) == '*': here are two sub conditions:
+ *      1. if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this
+ * case, a* only counts as empty
+ *      2. if p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.': dp[i][j] =
+ * dp[i-1][j]    //in this case, a* counts as multiple a or dp[i][j] =
+ * dp[i][j-1]   // in this case, a* counts as single a or dp[i][j] = dp[i][j-2]
+ * // in this case, a* counts as empty
  *
  * Date: Apr/25/2019
  * Author: Wei Du
@@ -26,29 +26,26 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
-
-        f[0][0] = true;
-        for (int i = 1; i <= m; i++) f[i][0] = false;
-        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and
-        // p[0..j - 3] matches empty
-        for (int j = 1; j <= n; j++)
-            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] != '*') {
-                    f[i][j] = f[i - 1][j - 1] &&
-                              (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+        // initialize table
+        std::vector<std::vector<bool>> table(
+            s.size() + 1, std::vector<bool>(p.size() + 1, false));
+        table[0][0] = true;
+        // table[0][1] = false;
+        for (int j = 1; j < p.size(); ++j)
+            table[0][j + 1] = table[0][j - 2] && p[j] == '*';
+        for (int i = 0; i < s.size(); ++i) {
+            for (int j = 0; j < p.size(); ++j) {
+                if (p[j] == '*') {
+                    table[i + 1][j + 1] =
+                        table[i + 1][j - 2] ||
+                        (table[i][j + 1] &&
+                         (p[j - 1] == s[i] || p[j - 1] == '.'));
                 } else {
-                    // p[0] cannot be '*' so no need to check "j > 1" here
-                    f[i][j] = f[i][j - 2] ||
-                              (s[i - 1] == p[j - 2] || '.' == p[j - 2]) &&
-                                  f[i - 1][j];
+                    table[i + 1][j + 1] =
+                        table[i][j] && (p[j] == s[i] || p[j] == '.');
                 }
             }
         }
-        return f[m][n];
+        return table[s.size()][p.size()];
     }
 };
