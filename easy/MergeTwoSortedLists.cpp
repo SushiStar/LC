2ea69fe414a5e057e453a5b83b8e5d09fac4d9a8
Wayne Du <ddwwzzyy@gamil.com>
2020-02-16 22:40:22 -0500

    merge two sorted lists

--- easy/MergeTwoSortedLists.cpp
+++ easy/MergeTwoSortedLists.cpp
@@ -1,13 +1,14 @@
-// Merge two sorted lists
-//
-
-/**
+/* Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ * 
  * Definition for singly-linked list.
  * struct ListNode {
  *      int val;
  *      ListNode *next;
  *      ListNode(int x) : val(x), next(null) {}
  * };
+ *
+ * Date: 02/16/2020
  */
 
 class Solution {
