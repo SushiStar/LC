18bb1af12b0b997306935d31f0339677743ac7f0
Wayne Du <ddwwzzyy@gamil.com>
2020-02-16 22:11:24 -0500

    valid parenthese

--- easy/ValidParentheses.cpp
+++ easy/ValidParentheses.cpp
@@ -1,53 +1,47 @@
 /*
- *Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
  *
- *An input string is valid if:
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
  *
- *Open brackets must be closed by the same type of brackets.
- *Open brackets must be closed in the correct order.
- *Note that an empty string is also considered valid.
- *
+ * Date: 02/16/2020
  */
 class Solution {
 public:
     bool isValid(string s) {
-        std::stack<char> stk;
-        for (int i = 0; i < s.size(); ++i) {
-            switch (s.at(i)) {
-                case '(':
-                 case '[':
-                 case '{':
-                    stk.push(s.at(i));
-                    break;
-                 case ')':
-                     if(stk.empty() || stk.top() != '('){
-                        return false;
-                    }else{
-                        stk.pop();
-                        break; 
-                    }
-                 case ']':
-                    if(stk.empty() || stk.top() != '['){
-                        return false;
-                    }else{
-                        stk.pop();
-                        break; 
-                    }
-                 case '}':
-                    if(stk.empty() || stk.top() != '{'){
-                        return false;
-                    }else{
-                        stk.pop();
-                        break; 
-                    }                   
-                default:
-                    continue;
+        std::stack<char> buffer;
+        for (auto ch : s) {
+            switch (ch) {
+            case '(': // fall through;
+            case '[':
+            case '{':
+                buffer.push(ch);
+                break;
+            case ')':
+                if (buffer.empty() || buffer.top() != '(')
+                    return false;
+                else
+                    buffer.pop();
+                break;
+            case ']':
+                if (buffer.empty() || buffer.top() != '[')
+                    return false;
+                else
+                    buffer.pop();
+                break;
+            case '}':
+                if (buffer.empty() || buffer.top() != '{')
+                    return false;
+                else
+                    buffer.pop();
+                break;
+            default:
+                break;
             }
-        } 
-        if(stk.size() == 0){
-            return true;
-        }else{
-            return false;
         }
+        return buffer.empty();
     }
 };
