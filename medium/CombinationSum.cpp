2551ad72525e33ccc9fc6079b4ddf874d54e48e1
wweedd <ddwwzzyy@gmail.com>
2020-02-23 18:34:36 -0500

    combination sum

--- medium/CombinationSum.cpp
+++ medium/CombinationSum.cpp
@@ -8,64 +8,36 @@
  * set must not contain duplicate combinations.
  * DP Get:)
  *
- * Date: Mar/28/2019
+ * for int i = 0; i < size; ++i
+ *      find combination k times of candidates[i] and the rest solution
+ *
+ * Date: 02/23/2020
  * Author: Wei Du
  */
 
 class Solution {
 public:
-    std::unordered_map<int,int> num2val;
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target){
-        std::vector<vector<int>> res;
-        if(candidates.empty()) return res;
-        // get how many times a number can be in the solution
-        for (auto num:candidates) num2val[num] = target/num;
-
-        int end = candidates.size() -1;
-        int left(0),right(end);
-        auto ress = getSumless(candidates,left,right,target);
-        for (auto temp:ress) {
-        }
-        return res;
-    }
-
-    vector<std::pair<vector<elmt>,int>> getSumless( vector<int>& candidates ,
-            int left, int right, int target) {
-        vector<std::pair<vector<elmt>, int>> res;
-        if (left == right) {
-            int num = candidates[left];
-            int times = num2val[num];
-            for (int i = 0; i <= times; ++i) {
-                elmt temp(num,i);
-                std::vector<elmt> a{temp};
-                res.push_back(std::make_pair(a, temp.val));
-            }
-        } else {
-            int mid = (left+right)/2;
-            auto res1 = getSumless(candidates, left, mid, target);
-            auto res2 = getSumless(candidates, mid+1, right,target);
-            for (auto m1:res1) {
-                for (auto m2:res2) {
-                    int sum = m1.second + m2.second;
-                    if (sum <= target) {
-                        auto p = m1.first;
-                        p.insert(p.end(), m2.first.begin(), m2.first.end());
-                        res.push_back(std::make_pair(p, sum));
-                    }
-                }
-            }
-
-        }
-        return res;
+    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
+        return combinationSum(candidates, target, 0);
     }
 private:
-    struct elmt {
-        elmt(int num_, int times_):num(num_),times(times_){
-            val = num*times;
+    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target, int start) {
+        if (!target) {
+            std::vector<std::vector<int>> rlt{{}};
+            return rlt;
         }
-        int num;
-        int times;
-        int val;
-    };
+        std::vector<std::vector<int>> rlt;
+        for (int i = start; i < candidates.size(); ++i) {
+            int numX = target/candidates[i];
+            if (!numX) continue;
+            for (int j = 1; j <= numX; ++j) {
+                auto rltNumX = combinationSum(candidates, target-j*candidates[i], i+1);
+                if (rltNumX.empty()) continue;
+                for (auto& vNumX : rltNumX) vNumX.insert(vNumX.end(), j, candidates[i]);
+                rlt.insert(rlt.end(), rltNumX.begin(), rltNumX.end());
+            }
+        }
+        return rlt;
+    }
 };
+
