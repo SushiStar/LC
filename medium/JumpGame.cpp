649dc183ca7634506e8481eef8e35339a334061c
wweedd <ddwwzzyy@gmail.com>
2020-03-07 16:19:34 -0500

    jumpgame

--- medium/JumpGame.cpp
+++ medium/JumpGame.cpp
@@ -5,28 +5,30 @@
  * at that position.
  * Determine if you are able to reach the last index.
  *
- * Date: Mar/29/2019
+ * Date: 03/07/2020
  *
  * Author: Wei Du
  */
 
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    bool canJump(vector<int> &nums) {
         int end = nums.size() - 1;
         if (end < 1) return true;
         if (nums[0] == 0) return false;
 
         int pos(0);
-        // try the maximum step every time,
-        // if not feasible(land at 0), go back steps
+        /*
+         * try the maximum step every time,
+         * if not feasible(land at 0), go back one step and try
+         * to cross the gap
+         */
         while (true) {
             pos = pos + nums[pos];
 
             if (pos >= end) return true;
-            // else  not reachable
 
-            if (nums[pos] == 0) {  // try to get across the gap
+            if (nums[pos] == 0) { // try to get across the gap
                 int flag(pos);
                 while (pos > 0 && pos + nums[pos] <= flag) {
                     --pos;
