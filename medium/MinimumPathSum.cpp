09cacfc8e57e7e136e9a9d8c8080bebe78a258cd
wweedd <ddwwzzyy@gmail.com>
2020-03-07 19:17:12 -0500

    minimum path sum

--- medium/MinimumPathSum.cpp
+++ medium/MinimumPathSum.cpp
@@ -5,7 +5,7 @@
  *
  * You can only move either down or right at any point in time.
  *
- * Date: Mar/30/2019
+ * Date: 03/07/2020
  *
  * Author: Wei Du
  *
@@ -13,7 +13,7 @@
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum(vector<vector<int>> &grid) {
         if (grid.empty()) return 0;
         int row = grid.size() - 1;
         int col = grid[0].size() - 1;
@@ -33,7 +33,7 @@ public:
                 int tpcst = fcst + grid[pos.first][pos.second];
                 if (tpcst < cost[pos.first][pos.second]) {
                     cost[pos.first][pos.second] = tpcst;
-                    q.push(pos);  // only push nodes could make contribution
+                    q.push(pos); // only push nodes could make contribution
                 }
             }
             if (f.second < col) {
@@ -41,7 +41,7 @@ public:
                 int tpcst = fcst + grid[pos.first][pos.second];
                 if (tpcst < cost[pos.first][pos.second]) {
                     cost[pos.first][pos.second] = tpcst;
-                    q.push(pos);  // only push nodes could make contribution
+                    q.push(pos); // only push nodes could make contribution
                 }
             }
         }
@@ -51,30 +51,50 @@ public:
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum(vector<vector<int>> &grid) {
         if (grid.empty()) return 0;
-        int row = grid.size()-1;
-        int col = grid[0].size()-1;
-        std::vector<int> m (col+1,INT_MAX);
-        std::vector<std::vector<int>> cost(row+1, m);
+        int row = grid.size() - 1;
+        int col = grid[0].size() - 1;
+        std::vector<int> m(col + 1, INT_MAX);
+        std::vector<std::vector<int>> cost(row + 1, m);
         cost[0][0] = grid[0][0];
 
-        int sum = row+col;
-        for (int op = 1; op <=sum; ++op){ 
+        int sum = row + col;
+        for (int op = 1; op <= sum; ++op) {
 
-            int r(0),c(0);
+            int r(0), c(0);
             if (op > row) {
                 r = row;
-                c = op-row;
+                c = op - row;
             }
-            while (c<=col){
-                if (r > 0) cost[r][c] = min(cost[r][c], cost[r-1][c]+grid[r][c]); 
-                if (c > 0) cost[r][c] = min(cost[r][c], cost[r][c-1]+grid[r][c]); 
+            while (c <= col) {
+                if (r > 0)
+                    cost[r][c] = min(cost[r][c], cost[r - 1][c] + grid[r][c]);
+                if (c > 0)
+                    cost[r][c] = min(cost[r][c], cost[r][c - 1] + grid[r][c]);
                 --r;
                 ++c;
             }
-
         }
         return cost[row][col];
+    }
+};
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>&grid) {
+        int col{grid[0].size()};
+        int row{grid.size()};
+        std::vector<std::vector<int>> rlt(row, std::vector<int>(col, INT_MAX));
+        rlt[0][0] = grid[0][0];
+        for (int i = 1; i < col; ++i)
+            rlt[0][i] = rlt[0][i-1] + grid[0][i];
+        for (int j = 1; j < row; ++j) 
+            rlt[j][0] = rlt[j-1][0] + grid[j][0];
+        for (int i= 1; i < row; ++i) {
+            for (int j = 1; j < col; ++j) 
+                rlt[i][j] = std::min(rlt[i-1][j], rlt[i][j-1]) + grid[i][j];
+        }
+        return rlt[row-1][col-1];
     }
 };
