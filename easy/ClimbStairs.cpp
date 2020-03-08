2c03ae5cca270f65e1af36b4763ae9b5bc79ecf0
wweedd <ddwwzzyy@gmail.com>
2020-03-07 19:25:22 -0500

    climb stairs

--- easy/ClimbStairs.cpp
+++ easy/ClimbStairs.cpp
@@ -3,7 +3,7 @@
  * Each time you can either climb 1 or 2 steps.
  * In how many distinct ways can you climb to the top?
  *
- * Date: Mar/30/2019
+ * Date: 03/07/2020
  *
  * Author: Wei Du
  *
@@ -11,14 +11,14 @@
 
 class Solution {
 public:
-    std::vector<int> res;
     int climbStairs(int n) {
-        if (n < 2) return 1;
-        res.push_back(1);
-        res.push_back(1);
-        for (int i = 2; i <=n; ++i) {
+        if (n < 3) return n;
+        std::vector<int> res(n+1);
+        res[1] = [1];
+        res[2] = [2];
+        for (int i = 3; i <=n; ++i) {
             res[i] = res[i-1]+res[i-2];
         }
-        return res[n];
+        return res.back();
     }
 };
