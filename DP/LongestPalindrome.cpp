5a789556d5fa8e33a3da18253823261961220544
Wayne Du <ddwwzzyy@gamil.com>
2020-03-11 12:52:20 -0400

    longestpalindrome

---
+++ DP/LongestPalindrome.cpp
@@ -0,0 +1,34 @@
+/*
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume the maximum length of the string is 1000.
+ *
+ * Date: 03/11/2020
+ * Author: Wei Du
+ */
+
+class Solution {
+public:
+    string longestPalindromic(string s) {
+        if (s.empty()) return s;
+        int start(0), len(0);
+        for (int i = 0; i < s.size(); ++i) {
+            int len1 = expandAroundCenter(i, i, s);
+            int len2 = expandAroundCenter(i, i + 1, s);
+            int thisLen = std::max(len1, len2);
+            if (len < thisLen) {
+                start = i - (thisLen - 1) / 2;
+                len = thisLen;
+            }
+        }
+        return s.substr(start, len);
+    }
+
+private:
+    int expandAroundCenter(int pl, int pr, const std::string &s) {
+        while (pl >= 0 && pr < s.size() && (s[pl] == s[pr])) {
+            --pl;
+            ++pr;
+        }
+        return pr - pl - 1;
+    }
+};
