a80cf5108ad29bb96f6d17ac001d4cbf4957bba4
Wayne Du <ddwwzzyy@gamil.com>
2020-03-14 21:31:06 -0400

    recover binary search tree

---
+++ Tree/RecoverBST.cpp
@@ -0,0 +1,71 @@
+/*
+ * Two elements of a binary search tree are swapped by mistake.
+ * Recover the tree without changing the structure.
+ *
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ *
+ * Date: 03/14/2020
+ * Author: Wei Du
+ */
+
+class Solution {
+public:
+    void recoverTree(TreeNode *root) {
+        std::vector<TreeNode *> inorder;
+        std::stack<TreeNode *> stk;
+        while (root || !stk.empty()) {
+            if (root) {
+                stk.push(root);
+                root = root->left;
+            } else {
+                root = stk.top();
+                stk.pop();
+                inorder.push_back(root);
+                root = root->right;
+            }
+        }
+        TreeNode *mark1{nullptr}, *mark2{nullptr};
+        for (int i = 1; i < inorder.size(); ++i) {
+            if (inorder[i]->val <= inorder[i - 1]->val) {
+                if (!mark1) {
+                    mark1 = inorder[i - 1];
+                    mark2 = inorder[i];
+                    continue;
+                } else {
+                    mark2 = inorder[i];
+                    break;
+                }
+            }
+        }
+        std::swap(mark1->val, mark2->val);
+    }
+};
+
+class Solution {
+public:
+    void recoverTree(TreeNode *root) {
+        checkSubTree(root);
+        std::swap(mark1->val, mark2->val);
+    }
+
+private:
+    TreeNode *prevNode{nullptr}, *mark1{nullptr}, *mark2{nullptr};
+    void checkSubTree(TreeNode *root) {
+        if (!root) return;
+        checkSubTree(root->left);
+        if (prevNode && prevNode->val >= root->val) {
+            if (!mark1) {
+                mark1 = prevNode;
+            }
+            mark2 = root;
+        }
+        prevNode = root;
+        checkSubTree(root->right);
+    }
+};
