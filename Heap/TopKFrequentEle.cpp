83e7996c7dbdb589f2af900d4b1bf981ee5c2b1b
wweedd <ddwwzzyy@gmail.com>
2020-03-31 23:55:46 -0400

    heap

---
+++ Heap/TopKFrequentEle.cpp
@@ -0,0 +1,30 @@
+/*
+ * Given a non-empty array of integers, return the k most frequent elements.
+ * You may assume k is always valid, 1<=k<=number of unique elements.
+ * Your algorithm's time complexity must be better than O(nlog(n)).
+ *
+ * Date: 03/31/2020
+ * Author: Wei Du
+ */
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::vector<int> res;
+        if (nums.empty()) return res;
+        std::unordered_map<int, int> mp;
+        auto cmp = [](std::pair<int,int> a, std::pair<int,int> b) {return a.second < b.second;};
+        std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>,decltype(cmp)> pq(cmp); 
+        for (auto num : nums) {
+            ++mp[num];
+        }
+        for (auto vl : mp) {
+            pq.push(vl);
+        }
+        for (int i = 0; i < k; ++i) {
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        return res;
+    }
+};
