af60d18fe09ce7f3142c0208e73a3a4e47a85549
wweedd <ddwwzzyy@gmail.com>
2020-04-10 01:48:31 -0400

    DP

---
+++ DP/DivisorGame.cpp
@@ -0,0 +1,31 @@
+/*
+ * Alice and Bob take turns playing a game, with Alice starting first.
+ * Initially, there is a number N on the chalkboard. On each player's
+ * turn, that player makes a move consisting of:
+ *      1. Choosing any x with 0 < x < N and N%x == 0.
+ *      2. Replacing the number N on the chalkboard with N-x.
+ * Also, if a player cannot make a move, they lose the game.
+ * Return True if and only if Alice wins the game, assuming both players
+ * play optimally.
+ *
+ * Date: 04/10/20202
+ * Author: Wei Du
+ */
+class Solution {
+public:
+    bool divisorGame(int N) {
+        if (N < 3) return N % 2 == 0;
+        std::vector<bool> dp(N + 1, false);
+        dp[1] = false;
+        dp[2] = true;
+        for (int i = 3; i < dp.size(); ++i) {
+            for (int j = 1; j < i / 2; ++j) {
+                if (i % j == 0 && !dp[i - j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
