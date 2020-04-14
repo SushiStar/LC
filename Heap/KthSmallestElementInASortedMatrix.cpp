9e65c772db5601fd2c2aef171990dce7222b24d3
wweedd <ddwwzzyy@gmail.com>
2020-04-14 18:09:50 -0400

    great

---
+++ Heap/KthSmallestElementInASortedMatrix.cpp
@@ -0,0 +1,44 @@
+/*
+ * Given a nxn matrix, where each of the rows and columns are sorted in
+ * ascending order, find the kth smallest element in the matrix.
+ * Note that it is the kth smallest element in the sorted order, not the
+ * kth distinct element.
+ *
+ * Date: 04/14/2020
+ * Author: Wei Du
+ */
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        if (matrix.empty() || matrix[0].empty()) return 0;
+        int n(matrix.size());
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                pq.push(matrix[i][j]);
+            }
+        }
+        --k;
+        while (k > 0) {
+            --k;
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        if (matrix.empty() || matrix[0].empty()) return 0;
+        int n(matrix.size());
+        std::vector<int> vct;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                vct.push_back(matrix[i][j]);
+            }
+        }
+        std::nth_element(vct.begin(), vct.begin() + (size_t)(k - 1), vct.end());
+        return vct[k - 1];
+    }
+};
