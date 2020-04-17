cbfcd55dc9edac1b1352c9c123b0f4c592bf7c92
wweedd <ddwwzzyy@gmail.com>
2020-04-16 23:20:56 -0400

    ./sdfj

---
+++ DFS_BFS/SumOfDistancesInTree.cpp
@@ -0,0 +1,11 @@
+/*
+ * An undirected, connected tree with N nodes labelled 0 ... N-1 and N-1 edegs
+ * are given.
+ * The ith edge connects nodes edges[i][0] and edges[i][1] together.
+ * Return a list ans, where ans[i] is the sum of the distances between node i and
+ * all other nodes.
+ *
+ * Date: 04/16/2020
+ * Author: Wei Du
+ */
+
