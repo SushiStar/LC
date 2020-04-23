07e4bdf193c3bbe5688cf5b38abc93d1417eb02a
wweedd <ddwwzzyy@gmail.com>
2020-04-23 01:40:33 -0400

    one more array

---
+++ Array/1bitand2bitCharacters.cpp
@@ -0,0 +1,26 @@
+/*
+ * We have two special characters. The first character can be
+ * represented by one bit 0. The second chaeracter can be represented
+ * by two bits (10 or 11).
+ *
+ * Now given a string represented by sevearl bits. Return whether the
+ * last character must be a one-bit character or not. The given string
+ * will always end with a zero.
+ *
+ * Date: 04/23/20020
+ * Author: Wei Du
+ */
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int> &bits) {
+        if (bits.size() == 1) return true;
+        int i = 0;
+        for (; i < bits.size() - 1; ++i) {
+            if (bits[i] == 0)
+                continue;
+            else
+                ++i;
+        }
+        return (i == bits.size() - 1);
+    }
+};
