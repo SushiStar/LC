0d280965169ec4c2b8be2bfb409cb1cb74b05a89
wweedd <ddwwzzyy@gmail.com>
2020-05-06 18:28:04 -0400

    wordbreak again

--- DP/WordBreakII.cpp
+++ DP/WordBreakII.cpp
@@ -9,6 +9,12 @@
  * Date: 04/12/2020
  * Author: Wei Du
  */
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
 
 // time limit exceeded
 class Solution {
@@ -45,7 +51,8 @@ private:
     }
 };
 
-class Solution {
+// time limit exceeded
+class Solution1 {
 public:
     vector<string> wordBreak(string s, vector<string> &wordDict) {
         std::vector<std::string> rlt{};
@@ -88,4 +95,37 @@ public:
     }
 };
 
+class Solution2 {
+private:
+    unordered_map<string, vector<string>> m;
 
+    vector<string> combine(string word, vector<string> prev) {
+        for (int i = 0; i < prev.size(); ++i) {
+            prev[i] += " " + word;
+        }
+        return prev;
+    }
+
+public:
+    vector<string> wordBreak(string s, std::vector<std::string> &wordDict) {
+        std::unordered_set<std::string> dict(wordDict.begin(), wordDict.end());
+        return wordBreak(s, dict);
+    }
+    std::vector<string> wordBreak(std::string s,
+                                  const std::unordered_set<std::string> &dict) {
+        if (m.count(s)) return m[s];
+        vector<string> result;
+        if (dict.count(s)) result.push_back(s);
+
+        for (int i = 1; i < s.size(); ++i) {
+            string word = s.substr(i);
+            if (dict.count(word)) {
+                string rem = s.substr(0, i);
+                vector<string> prev = combine(word, wordBreak(rem, dict));
+                result.insert(result.end(), prev.begin(), prev.end());
+            }
+        }
+        m[s] = result; // memorize
+        return result;
+    }
+};
