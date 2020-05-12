27723d70e55dbaced0ac6ffd9e81b8b0430eb341
wweedd <ddwwzzyy@gmail.com>
2020-05-12 14:28:17 -0400

    better

--- Array/SingleElementInASortedArray.cpp
+++ Array/SingleElementInASortedArray.cpp
@@ -42,3 +42,20 @@ private:
         return left_single;
     }
 };
+
+class Solution1 {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left(0), right(nums.size()-1);
+        int mid = left+(right-left)/2;
+        while (left < right) {
+            if ((mid %2 == 0 && nums[mid] == nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+            mid = left+(right-left)/2;
+        }
+        return nums[mid];
+    }
+};
