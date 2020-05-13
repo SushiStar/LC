5bfc01c3ebf99e0f63b2680568f0a580d39c1988
wweedd <ddwwzzyy@gmail.com>
2020-05-13 16:41:58 -0400

    what a great update

--- String/RemoveKDigits.cpp
+++ String/RemoveKDigits.cpp
@@ -9,8 +9,8 @@
  * Author: Wei Du
  */
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 // TLE
@@ -19,21 +19,25 @@ public:
     string removeKdigits(string num, int k) {
         std::string ret = removeAction(num, k);
         if (ret.empty()) ret += '0';
-        while (ret.size() > 1 && ret[0] == '0') ret.erase(0,1);
+        while (ret.size() > 1 && ret[0] == '0')
+            ret.erase(0, 1);
         return ret;
     }
+
 private:
     std::unordered_map<std::string, std::unordered_map<int, std::string>> dp;
-    string removeAction(std::string num, int k ) {
-        if (k == 0) return num;
-        else if (k == num.size()) return "";
+    string removeAction(std::string num, int k) {
+        if (k == 0)
+            return num;
+        else if (k == num.size())
+            return "";
         if (dp[num].find(k) != dp[num].end()) return dp[num][k];
         std::string ret{num};
-        for (int i = 0; i <= num.size()-k; ++i) {
+        for (int i = 0; i <= num.size() - k; ++i) {
             std::string strLeft = num.substr(0, i);
-            std::string strRight = num.substr(i+1);
-            std::string tmpRet = strLeft + removeAction(strRight, k-1);
-            if (tmpRet.size() < ret.size() ||  tmpRet < ret) {
+            std::string strRight = num.substr(i + 1);
+            std::string tmpRet = strLeft + removeAction(strRight, k - 1);
+            if (tmpRet.size() < ret.size() || tmpRet < ret) {
                 ret = tmpRet;
             }
         }
@@ -47,21 +51,46 @@ public:
     string removeKdigits(string num, int k) {
         std::string ret = removeAction(num, k);
         if (ret.empty()) ret += '0';
-        while (ret.size() > 1 && ret[0] == '0') ret.erase(0,1);
+        while (ret.size() > 1 && ret[0] == '0')
+            ret.erase(0, 1);
         return ret;
     }
 
 private:
-    std::string removeAction(std::string num, int k ) {
+    std::string removeAction(std::string num, int k) {
         if (k == num.size())
             return "";
         else if (k <= 0)
             return num;
         size_t i = 0;
-        while ( i  < num.size()-k && num[i] <= num[i+1]) {
+        while (i < num.size() - k && num[i] <= num[i + 1]) {
             ++i;
         }
-        num = num.erase(std::min(i, num.size()-1),1);
-        return removeAction(num, k-1);
+        num = num.erase(std::min(i, num.size() - 1), 1);
+        return removeAction(num, k - 1);
+    }
+};
+
+class Solution2 {
+public:
+    string removeKdigits(string num, int k) {
+        if (k == num.size()) return "0";
+
+        std::string ret{num[0]};
+        for (int i = 1; i < num.size(); ++i) {
+            while (k > 0 && !ret.empty() && num[i] < ret.back()) {
+                ret.pop_back();
+                --k;
+            }
+            ret += num[i];
+        }
+        while (k > 0 && !ret.empty()) {
+            ret.pop_back();
+            --k;
+        }
+        while (!ret.empty() && ret[0] == '0')
+            ret.erase(0, 1);
+        if (ret.empty()) ret += '0';
+        return ret;
     }
 };
