ff65a86cc14c62db614a02382412dee627cfaec1
wweedd <ddwwzzyy@gmail.com>
2020-05-30 17:26:49 -0400

    fuck the riots

---
+++ DFS_BFS/ShortestPathVisitingAllNodes.cpp
@@ -0,0 +1,22 @@
+/*
+ * An undirected, connected graph of N nodes (labeled 0, 1, 2, ..., N-1) is given
+ * as graph. graph.length = N, and j != i is in the list graph[i] exactly once, if
+ * and only if nodes i and j are connected.
+ * Return the length of the shortest path that visits every node. You may start and 
+ * stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+ *
+ * 1 <= graph.length <= 12
+ * 0 <= graph[i].length < graph.length
+ *
+ * Date: 05/29/2020
+ * Author: Wei Du
+ */
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        
+    }
+};
