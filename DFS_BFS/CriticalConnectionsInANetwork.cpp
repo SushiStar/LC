e7c95dbe01b33aaf52fc0b378a9d4549a67d6caa
wweedd <ddwwzzyy@gmail.com>
2020-06-09 17:19:34 -0700

    articulation points

--- DFS_BFS/CriticalConnectionsInANetwork.cpp
+++ DFS_BFS/CriticalConnectionsInANetwork.cpp
@@ -10,16 +10,20 @@
  * Date: 03/24/2020
  * Author: Wei Du
  */
+#include <vector>
+using namespace std;
 
+// articulation bridge
 class Solution {
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+    vector<vector<int>> criticalConnections(int n,
+                                            vector<vector<int>> &connections) {
         if (n < 2) return connections;
         std::vector<std::vector<int>> result{};
-    
+
         // initialize
-        std::vector<vertex*> vertices(n, nullptr);
-        for (int i = 0; i < n; ++i) 
+        std::vector<vertex *> vertices(n, nullptr);
+        for (int i = 0; i < n; ++i)
             vertices[i] = new vertex(i);
         int idx = 0;
         for (auto &edge : connections) {
@@ -33,33 +37,33 @@ public:
         }
         vertices[0]->timeStamp = 0;
         dfs(vertices[0], nullptr, connections);
-        
+
         for (auto &edge : connections) {
-            if (edge.size() == 2){ 
+            if (edge.size() == 2) {
                 result.push_back(edge);
             }
         }
-        
+
         return result;
     }
+
 private:
     struct vertex {
-        vertex(int val_) : val(val_), timeStamp(-1), neighbors(){}
+        vertex(int val_) : val(val_), timeStamp(-1), neighbors() {}
         int val;
         int timeStamp;
-        std::vector<vertex*> neighbors;
+        std::vector<vertex *> neighbors;
         std::vector<int> edgePtr;
     };
-    
-    int dfs(vertex* curr, vertex* parent, vector<vector<int>>& connections) {
-        
+
+    int dfs(vertex *curr, vertex *parent, vector<vector<int>> &connections) {
         int result{curr->timeStamp};
-        for (int i = 0; i < curr->neighbors.size(); ++i){
+        for (int i = 0; i < curr->neighbors.size(); ++i) {
             auto nb = curr->neighbors[i];
             if (nb == parent) continue;
-            int circle {INT_MAX};
-            if (nb->timeStamp == -1){ 
-                nb->timeStamp = curr->timeStamp +1;
+            int circle{INT_MAX};
+            if (nb->timeStamp == -1) {
+                nb->timeStamp = curr->timeStamp + 1;
                 circle = dfs(nb, curr, connections);
             } else {
                 circle = nb->timeStamp;
@@ -68,8 +72,55 @@ private:
                 // remove edge mark [curr, nb] / [nb, curr];
                 connections[curr->edgePtr[i]].push_back(-1);
                 result = std::min(result, circle);
-            } 
+            }
         }
         return result;
     }
+};
+
+class Solution2 {
+public:
+    std::vector<vector<int>>
+		criticalConnections(int n, vector<vector<int>> &connections) {
+			dfs_low.resize(n,-1);
+			dfs_num.resize(n,-1);
+			dfs_parent.resize(n, -1);
+			neighbors.resize(n);
+			for (int i = 0; i < connections.size(); ++i) {
+				neighbors[connections[i][0]].push_back(connections[i][1]);
+				neighbors[connections[i][1]].push_back(connections[i][0]);
+			}
+			for (int i = 0; i < n; ++i) {
+				if (dfs_num[i] == -1) {	// un-visited
+					bridgeFinder(i);
+				}
+			}
+			return bridge;
+		}
+private:
+	using vcti = std::vector<int>;
+	using vvcti = std::vector<std::vector<int>>;
+	int dfsNumberCounter{0};
+	vcti dfs_low;
+	vcti dfs_num;
+	vcti dfs_parent;
+	vvcti neighbors;
+	vvcti bridge;
+
+	void bridgeFinder(int u) {
+		dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
+		for (auto v : neighbors[u]) {
+			dfs_parent[v] = u;
+			if (dfs_num[v] == -1) {	// unvisited
+				bridgeFinder(v);
+				if (dfs_low[v] > dfs_num[u])
+					bridge.push_back({u,v});
+				else {	// v has reached a ancestor
+					dfs_low[u] = std::min(dfs_low[u], dfs_low[v]);
+				}
+			} else if (dfs_parent[u] != v) {
+				dfs_low[u] = std::min(dfs_low[u], dfs_num[v]);
+			}
+		}
+	}
 };
