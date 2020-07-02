78daa527480c86eacbc0f8609c814fafb553d62f
wweedd <ddwwzzyy@gmail.com>
2020-07-02 14:17:11 -0700

    sdf

--- DFS_BFS/BinaryTreeLevelOrderTraversalII.cpp
+++ DFS_BFS/BinaryTreeLevelOrderTraversalII.cpp
@@ -1,21 +1,22 @@
 /*
- * Definition of a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode* left;
- *     TreeNode* right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- *
  * Date: 03/25/2020
  *
  * Author: Wei Du
  */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        std::vector<std::vector<int>> result; 
+        std::vector<std::vector<int>> result;
         if (!root) return result;
         std::vector<TreeNode*> buffer[2];
         int idx0{0};
@@ -34,5 +35,29 @@ public:
         }
         std::reverse(result.begin(), result.end());
         return result;
+    }
+};
+
+class Solution1 {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+      if (!root) return {};
+      vector<vector<int>> ret{};
+      vector<TreeNode*> bfs{root};
+      int idx{0}, mark{1};
+      ret.push_back({});
+      while (idx < bfs.size()) {
+        auto tmp = bfs[idx++];
+        ret.back().push_back(tmp->val);
+        if (tmp->left) bfs.push_back(tmp->left);
+        if (tmp->right) bfs.push_back(tmp->right);
+        if (idx == mark) {
+          ret.push_back({});
+          mark = bfs.size();
+        }
+      }
+      ret.pop_back();
+      std::reverse(ret.begin(), ret.end());
+      return ret;
     }
 };
