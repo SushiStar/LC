a0d3eed392bf682131a1c612c72ff08da1634f77
wweedd <ddwwzzyy@gmail.com>
2020-07-19 15:34:58 -0700

    add include

--- Graph/LongestIncreasingPathInAMatrix.cpp
+++ Graph/LongestIncreasingPathInAMatrix.cpp
@@ -8,52 +8,51 @@
  */
 
 #include <vector>
-
 using namespace std;
 
 class Solution {
-public:
-    int longestIncreasingPath(vector<vector<int>> &matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
-        depth.resize(matrix.size(), std::vector<int>(matrix[0].size(), -1));
-        // Do BFS  with memo
-        int ret{1};
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
-                coord curr{i, j};
-                ret = std::max(ret, bfs(matrix, curr));
-            }
-        }
-        return ret;
+ public:
+  int longestIncreasingPath(vector<vector<int>> &matrix) {
+    if (matrix.empty() || matrix[0].empty()) return 0;
+    depth.resize(matrix.size(), std::vector<int>(matrix[0].size(), -1));
+    // Do BFS  with memo
+    int ret{1};
+    for (int i = 0; i < matrix.size(); ++i) {
+      for (int j = 0; j < matrix[0].size(); ++j) {
+        coord curr{i, j};
+        ret = std::max(ret, bfs(matrix, curr));
+      }
     }
+    return ret;
+  }
 
-private:
-    typedef std::pair<int, int> coord;
-    std::vector<std::vector<int>> depth;
+ private:
+  typedef std::pair<int, int> coord;
+  std::vector<std::vector<int>> depth;
 
-    int bfs(const std::vector<std::vector<int>> &matrix, coord &curr) {
-        if (depth[curr.first][curr.second] != -1)
-            return depth[curr.first][curr.second];
-        int ret{1};
-        int row(curr.first), col(curr.second);
-        int currVal{matrix[row][col]};
-        if (row > 0 && matrix[row - 1][col] > currVal) {
-            coord neighbor{row - 1, col};
-            ret = std::max(ret, bfs(matrix, neighbor) + 1);
-        }
-        if (row < matrix.size() - 1 && matrix[row + 1][col] > currVal) {
-            coord neighbor{row + 1, col};
-            ret = std::max(ret, bfs(matrix, neighbor) + 1);
-        }
-        if (col > 0 && matrix[row][col - 1] > currVal) {
-            coord neighbor{row, col - 1};
-            ret = std::max(ret, bfs(matrix, neighbor) + 1);
-        }
-        if (col < matrix[0].size() - 1 && matrix[row][col + 1] > currVal) {
-            coord neighbor{row, col + 1};
-            ret = std::max(ret, bfs(matrix, neighbor) + 1);
-        }
-        depth[row][col] = ret;
-         return ret;
+  int bfs(const std::vector<std::vector<int>> &matrix, coord &curr) {
+    if (depth[curr.first][curr.second] != -1)
+      return depth[curr.first][curr.second];
+    int ret{1};
+    int row(curr.first), col(curr.second);
+    int currVal{matrix[row][col]};
+    if (row > 0 && matrix[row - 1][col] > currVal) {
+      coord neighbor{row - 1, col};
+      ret = std::max(ret, bfs(matrix, neighbor) + 1);
     }
+    if (row < matrix.size() - 1 && matrix[row + 1][col] > currVal) {
+      coord neighbor{row + 1, col};
+      ret = std::max(ret, bfs(matrix, neighbor) + 1);
+    }
+    if (col > 0 && matrix[row][col - 1] > currVal) {
+      coord neighbor{row, col - 1};
+      ret = std::max(ret, bfs(matrix, neighbor) + 1);
+    }
+    if (col < matrix[0].size() - 1 && matrix[row][col + 1] > currVal) {
+      coord neighbor{row, col + 1};
+      ret = std::max(ret, bfs(matrix, neighbor) + 1);
+    }
+    depth[row][col] = ret;
+    return ret;
+  }
 };
--- Graph/MinimumHeightTrees.cpp
+++ Graph/MinimumHeightTrees.cpp
@@ -16,39 +16,39 @@
  * Date: 04/15/2020
  * Author: Wei Du
  */
-
+#include "../common.h"
 // it is assumed no circle exist
 // BFS is too slow
 
 // remove leaves layer by layer, the rest 1 or two nodes could be the answer
 class Solution {
-public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>> &edges) {
-        if (n == 1) return std::vector<int>{0};
-        if (n == 2) return std::vector<int>{0, 1};
-        std::unordered_map<int, std::unordered_set<int>> neighbors;
-        std::unordered_set<int> remover;
-        for (auto &e : edges) {
-            remover.insert(e[0]);
-            remover.insert(e[1]);
-            neighbors[e[0]].insert(e[1]);
-            neighbors[e[1]].insert(e[0]);
-        }
-        while (remover.size() > 2) {
-            std::vector<int> buffer;
-            for (auto curr : remover) {
-                if (neighbors[curr].size() == 1) buffer.push_back(curr);
-            }
-            for (int leaf : buffer) {
-                if (!neighbors[leaf].empty()) {
-                    int n = *(neighbors[leaf].begin());
-                    neighbors[n].erase(leaf);
-                    neighbors[leaf].clear();
-                }
-                remover.erase(leaf);
-            }
-        }
-        std::vector<int> rlt(remover.begin(), remover.end());
-        return rlt;
+ public:
+  vector<int> findMinHeightTrees(int n, vector<vector<int>> &edges) {
+    if (n == 1) return std::vector<int>{0};
+    if (n == 2) return std::vector<int>{0, 1};
+    std::unordered_map<int, std::unordered_set<int>> neighbors;
+    std::unordered_set<int> remover;
+    for (auto &e : edges) {
+      remover.insert(e[0]);
+      remover.insert(e[1]);
+      neighbors[e[0]].insert(e[1]);
+      neighbors[e[1]].insert(e[0]);
     }
+    while (remover.size() > 2) {
+      std::vector<int> buffer;
+      for (auto curr : remover) {
+        if (neighbors[curr].size() == 1) buffer.push_back(curr);
+      }
+      for (int leaf : buffer) {
+        if (!neighbors[leaf].empty()) {
+          int n = *(neighbors[leaf].begin());
+          neighbors[n].erase(leaf);
+          neighbors[leaf].clear();
+        }
+        remover.erase(leaf);
+      }
+    }
+    std::vector<int> rlt(remover.begin(), remover.end());
+    return rlt;
+  }
 };
