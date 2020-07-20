ef8f2c4752125c90484a0714d2b4e0361033263a
wweedd <ddwwzzyy@gmail.com>
2020-07-19 21:17:15 -0700

    updated

--- Hash_Table/GroupShiftedStrings.cpp
+++ Hash_Table/GroupShiftedStrings.cpp
@@ -11,11 +11,27 @@
 #include "../common.h"
 
 class Solution {
+  string getKey(const string &str) {
+    string ret{};
+    for (int i = 1; i < str.size(); ++i) {
+      int diff = (int)str[i]-(int)str[i-1];
+      if (diff < 0) diff +=26;
+      ret.push_back('a'+diff);
+    }
+    return ret;
+  }
 public:
     vector<vector<string>> groupStrings(vector<string>& strings) {
       vector<vector<string>> ret{};
+      unordered_map<string, int> umap;
       for (const string &str : strings) {
-
+        string key = getKey(str);
+        if (!umap.count(key)) {
+          umap[key] = ret.size();
+          ret.push_back({str});
+        } else {
+          ret[umap[key]].push_back(str);
+        }
       }
       return ret;
     }
