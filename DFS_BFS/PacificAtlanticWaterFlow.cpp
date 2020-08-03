e34a52a895087063f4b41af4edab1df5e7824065
wweedd <ddwwzzyy@gmail.com>
2020-08-03 15:13:01 -0700

    updated flow

--- DFS_BFS/PacificAtlanticWaterFlow.cpp
+++ DFS_BFS/PacificAtlanticWaterFlow.cpp
@@ -20,96 +20,71 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
-        if (matrix.empty()) {
-            return vector<vector<int>>();
-        }
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
-        vector<vector<bool>> pacific(n, vector<bool>(m, false));
-        
-        for (int i = 0; i < n; ++i) {
-            if (!pacific[i][0]) {
-                flowPacific(i, 0, matrix, pacific);
-            }
-            
-            if (!atlantic[i][m - 1]) {
-                flowAtlantic(i, m - 1, matrix, atlantic);
-            }
-        }
-        
-        for (int i = 0; i < m; ++i) {
-            if (!pacific[0][i]) {
-                flowPacific(0, i, matrix, pacific);
-            }
-            
-            if (!atlantic[n - 1][i]) {
-                flowAtlantic(n - 1, i, matrix, atlantic);
-            }
-        }
-        
-        vector<vector<int>> result;
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (atlantic[i][j] && pacific[i][j]) {
-                    result.push_back(vector<int>{i, j});
-                }
-            }
-        }
-        
-        return result;
+ public:
+  vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
+    if (matrix.empty()) return vector<vector<int>>();
+
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<vector<bool>> atlantic(n, vector<bool>(m, false));
+    vector<vector<bool>> pacific(n, vector<bool>(m, false));
+
+    for (int i = 0; i < n; ++i) {
+      if (!pacific[i][0]) flowPacific(i, 0, matrix, pacific);
+      if (!atlantic[i][m - 1]) flowAtlantic(i, m - 1, matrix, atlantic);
     }
-    
-private:
-    
-    void flowPacific(int i, int j, const vector<vector<int>>& matrix, vector<vector<bool>>& pacific) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        pacific[i][j] = true;
-        
-        if (i - 1 >= 0 && !pacific[i - 1][j] && matrix[i - 1][j] >= matrix[i][j]) {
-            flowPacific(i - 1, j, matrix, pacific);
-        }
-        
-        if (i + 1 < n && !pacific[i + 1][j] && matrix[i + 1][j] >= matrix[i][j]) {
-            flowPacific(i + 1, j, matrix, pacific);
-        }
-        
-        if (j - 1 >= 0 && !pacific[i][j - 1] && matrix[i][j - 1] >= matrix[i][j]) {
-            flowPacific(i, j - 1, matrix, pacific);
-        }
-        
-        if (j + 1 < m && !pacific[i][j + 1] && matrix[i][j + 1] >= matrix[i][j]) {
-            flowPacific(i, j + 1, matrix, pacific);
-        }
+
+    for (int i = 0; i < m; ++i) {
+      if (!pacific[0][i]) flowPacific(0, i, matrix, pacific);
+      if (!atlantic[n - 1][i]) flowAtlantic(n - 1, i, matrix, atlantic);
     }
-    
-    void flowAtlantic(int i, int j, const vector<vector<int>>& matrix, vector<vector<bool>>& atlantic) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        atlantic[i][j] = true;
-        
-        if (i - 1 >= 0 && !atlantic[i - 1][j] && matrix[i - 1][j] >= matrix[i][j]) {
-            flowAtlantic(i - 1, j, matrix, atlantic);
-        }
-        
-        if (i + 1 < n && !atlantic[i + 1][j] && matrix[i + 1][j] >= matrix[i][j]) {
-            flowAtlantic(i + 1, j, matrix, atlantic);
-        }
-        
-        if (j - 1 >= 0 && !atlantic[i][j - 1] && matrix[i][j - 1] >= matrix[i][j]) {
-            flowAtlantic(i, j - 1, matrix, atlantic);
-        }
-        
-        if (j + 1 < m && !atlantic[i][j + 1] && matrix[i][j + 1] >= matrix[i][j]) {
-            flowAtlantic(i, j + 1, matrix, atlantic);
-        }
-    }
+
+    vector<vector<int>> result;
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j)
+        if (atlantic[i][j] && pacific[i][j])
+          result.push_back(vector<int>{i, j});
+
+    return result;
+  }
+
+ private:
+  void flowPacific(int i, int j, const vector<vector<int>>& matrix,
+                   vector<vector<bool>>& pacific) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    pacific[i][j] = true;
+
+    if (i - 1 >= 0 && !pacific[i - 1][j] && matrix[i - 1][j] >= matrix[i][j])
+      flowPacific(i - 1, j, matrix, pacific);
+
+    if (i + 1 < n && !pacific[i + 1][j] && matrix[i + 1][j] >= matrix[i][j])
+      flowPacific(i + 1, j, matrix, pacific);
+
+    if (j - 1 >= 0 && !pacific[i][j - 1] && matrix[i][j - 1] >= matrix[i][j])
+      flowPacific(i, j - 1, matrix, pacific);
+
+    if (j + 1 < m && !pacific[i][j + 1] && matrix[i][j + 1] >= matrix[i][j])
+      flowPacific(i, j + 1, matrix, pacific);
+  }
+
+  void flowAtlantic(int i, int j, const vector<vector<int>>& matrix,
+                    vector<vector<bool>>& atlantic) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    atlantic[i][j] = true;
+
+    if (i - 1 >= 0 && !atlantic[i - 1][j] && matrix[i - 1][j] >= matrix[i][j])
+      flowAtlantic(i - 1, j, matrix, atlantic);
+
+    if (i + 1 < n && !atlantic[i + 1][j] && matrix[i + 1][j] >= matrix[i][j])
+      flowAtlantic(i + 1, j, matrix, atlantic);
+
+    if (j - 1 >= 0 && !atlantic[i][j - 1] && matrix[i][j - 1] >= matrix[i][j])
+      flowAtlantic(i, j - 1, matrix, atlantic);
+
+    if (j + 1 < m && !atlantic[i][j + 1] && matrix[i][j + 1] >= matrix[i][j])
+      flowAtlantic(i, j + 1, matrix, atlantic);
+  }
 };
