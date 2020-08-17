3de9dd681bb1ef75919f24bc6544195f53831a39
wweedd <ddwwzzyy@gmail.com>
2020-08-16 22:36:32 -0700

    formatted LongestCommonSubsequence

--- DP/LongestCommonSubsequence.cpp
+++ DP/LongestCommonSubsequence.cpp
@@ -16,18 +16,18 @@
  */
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        std::vector<std::vector<int>> dp(text1.size() + 1,
-                                         std::vector<int>(text2.size() + 1, 0));
-        for (int i = 0; i < text1.size(); ++i) {
-            for (int j = 0; j < text2.size(); ++j) {
-                if (text1[i] == text2[j]) {
-                    dp[i + 1][j + 1] = dp[i][j] + 1;
-                } else {
-                    dp[i + 1][j + 1] = std::max(dp[i][j + 1], dp[i + 1][j]);
-                }
-            }
+  int longestCommonSubsequence(string text1, string text2) {
+    std::vector<std::vector<int>> dp(text1.size() + 1,
+                                     std::vector<int>(text2.size() + 1, 0));
+    for (int i = 0; i < text1.size(); ++i) {
+      for (int j = 0; j < text2.size(); ++j) {
+        if (text1[i] == text2[j]) {
+          dp[i + 1][j + 1] = dp[i][j] + 1;
+        } else {
+          dp[i + 1][j + 1] = std::max(dp[i][j + 1], dp[i + 1][j]);
         }
-        return dp.back().back();
+      }
     }
+    return dp.back().back();
+  }
 };
