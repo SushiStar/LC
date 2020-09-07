0aed5ee2f1a0448fa6505aefea4558416536893c
wweedd <ddwwzzyy@gmail.com>
2020-09-07 01:15:07 -0700

    updated 4sum

--- TwoPointers/4Sum.cpp
+++ TwoPointers/4Sum.cpp
@@ -8,41 +8,36 @@
  * Author: Wei Du
  *
  */
+#include "../common.h"
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> total;
-        int n = nums.size();
-        if (n < 4) return total;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n-3; ++i ) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            if (nums[i] + nums[i+i] + nums[i+2] + nums[i+3] > target) break;
-            if (nums[i] + nums[n-3] + nums[n-2] + nums[n-1] < target) continue;
-
-            for (int j = i+1; j < n-2; ++j ) {
-                if(j > 0 && nums[j] == nums[j-1]) continue;
-                if(nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
-                if(nums[i] + nums[j] + nums[n-2] + nums[n-1] < target) continue;
-                int left(j+1);
-                int right(n-1);
-                while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum < target) {
-                        ++left;
-                    } else if (sum > target) {
-                        --right;
-                    } else {
-                        total.push_back(vector<int>{nums[i],nums[j],nums[left],nums[right]});
-                        ++left;
-                        --right;
-                        while(left < right && nums[left] == nums[left-1]) ++left;
-                        while(left < right && nums[right] == nums[right+1]) --right;
-                    }
-                } // while
-            } // inner for
-        } // outer for
-        return total;
-    } 
+  vector<vector<int>> fourSum(vector<int> &nums, int target) {
+    std::vector<std::vector<int>> result{};
+    if (nums.size() <= 3) return result;
+    std::sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 3; ++i) {
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
+      for (int j = i + 1; j < nums.size() - 2; ++j) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        int left{j + 1}, right(nums.size() - 1);
+        while (left < right) {
+          auto total = nums[i] + nums[j] + nums[left] + nums[right];
+          if (total == target) {
+            result.push_back(
+                std::vector<int>{nums[i], nums[j], nums[left], nums[right]});
+            ++left;
+            --right;
+            while (left < right && nums[right] == nums[right + 1]) --right;
+            while (left < right && nums[left] == nums[left - 1]) ++left;
+          } else if (total > target) {
+            --right;
+          } else {
+            ++left;
+          }
+        }
+      }
+    }
+    return result;
+  }
 };
