b3a55d98c855d6112571b5bd88339ae936c2e61f
Wayne Du <ddwwzzyy@gmail.com>
2022-05-16 22:47:42 -0700

    better idea

--- Stack/MinimumRemoveToMakeValidParentheses.cpp
+++ Stack/MinimumRemoveToMakeValidParentheses.cpp
@@ -17,30 +17,64 @@
 using namespace std;
 
 class Solution {
-public:
-    string minRemoveToMakeValid(string s) {
-        std::vector<int> stk;
-        stk.reserve(s.size());
-        std::vector<bool> valid(s.size(), true);
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(' || s[i] == ')') {
-                if (!stk.empty() && s[stk.back()] == '(' && s[i] == ')') {
-                    stk.pop_back();
-                } else {
-                    stk.push_back(i);
-                }
-            }
+ public:
+  string minRemoveToMakeValid(string s) {
+    std::vector<int> stk;
+    stk.reserve(s.size());
+    std::vector<bool> valid(s.size(), true);
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == '(' || s[i] == ')') {
+        if (!stk.empty() && s[stk.back()] == '(' && s[i] == ')') {
+          stk.pop_back();
+        } else {
+          stk.push_back(i);
         }
-        for (int i = 0; i < stk.size(); ++i) {
-            valid[stk[i]] = false;
-        }
-        std::string ret;
-        ret.reserve(s.size());
-        for (int i = 0; i < s.size(); ++i) {
-            if (valid[i]) {
-                ret+=s[i];
-            }
-        }
-        return ret;
+      }
     }
+    for (int i = 0; i < stk.size(); ++i) {
+      valid[stk[i]] = false;
+    }
+    std::string ret;
+    ret.reserve(s.size());
+    for (int i = 0; i < s.size(); ++i) {
+      if (valid[i]) {
+        ret += s[i];
+      }
+    }
+    return ret;
+  }
+};
+
+class Solution2 {
+ public:
+  string minRemoveToMakeValid(string s) {
+    std::vector<int> pos;
+    for (auto i = 0; i < s.size(); ++i) {
+      if (s[i] == '(') {
+        pos.push_back(i);
+      } else if (s[i] == ')') {
+        if (pos.empty() || s[pos.back()] == ')') {
+          pos.push_back(i);
+        } else {
+          pos.pop_back();
+        }
+      }
+    }
+    auto mark = pos.begin();
+    std::string ret;
+    ret.reserve(s.size());
+    for (auto i = 0; i < s.size(); ++i) {
+      if (mark == pos.end()) {
+        ret.push_back(s[i]);
+        continue;
+      }
+      if (i < *mark) {
+        ret.push_back(s[i]);
+      } else {
+        ++mark;
+      }
+    }
+
+    return ret;
+  }
 };
